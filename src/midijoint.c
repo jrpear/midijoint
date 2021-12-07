9c7dd4881992d891538b0ac609014673b5f57caf
Jack Pearson <jack@pearson.onl>
2021-12-07 12:41:01 -0800

    add hardcoded map of note_on 69 to note_on 70

--- src/midijoint.c
+++ src/midijoint.c
@@ -1,13 +1,15 @@
 #include <stdlib.h>
 
-#include <lv2/core/lv2.h>
-#include <lv2/core/lv2_util.h>
 #include <lv2/atom/atom.h>
 #include <lv2/atom/util.h>
-#include <lv2/urid/urid.h>
+#include <lv2/core/lv2.h>
+#include <lv2/core/lv2_util.h>
 #include <lv2/log/log.h>
 #include <lv2/log/logger.h>
+#include <lv2/midi/midi.h>
+#include <lv2/urid/urid.h>
 
+#include "msgrel.h"
 #include "uris.h"
 
 #define MIDIJOINT_URI "https://github.com/jrpear/midijoint"
@@ -24,6 +26,7 @@ typedef struct {
   LV2_Log_Logger logger;
   MJPorts ports;
   MJURIs uris;
+  VMBinRel rel;
 } Midijoint;
 
 static void connect_port(LV2_Handle instance, uint32_t port, void *data) {
@@ -48,7 +51,7 @@ static LV2_Handle instantiate(const LV2_Descriptor *descriptor, double rate,
 
   const char *missing =
       lv2_features_query(features, LV2_URID__map, &self->map_data, true,
-          LV2_LOG__log, &self->logger.log, true, NULL);
+                         LV2_LOG__log, &self->logger.log, true, NULL);
 
   lv2_log_logger_set_map(&self->logger, self->map_data);
 
@@ -59,6 +62,12 @@ static LV2_Handle instantiate(const LV2_Descriptor *descriptor, double rate,
   }
 
   map_midijoint_uris(self->map_data, &self->uris);
+
+  self->rel.pairs[0].active = true;
+  self->rel.pairs[0].in.statchan = LV2_MIDI_MSG_NOTE_ON;
+  self->rel.pairs[0].in.data = 69;
+  self->rel.pairs[0].out.statchan = LV2_MIDI_MSG_NOTE_ON;
+  self->rel.pairs[0].out.data = 70;
 
   return (LV2_Handle)self;
 }
@@ -73,7 +82,23 @@ static void run(LV2_Handle instance, uint32_t sample_count) {
   self->ports.midi_out->atom.type = self->ports.midi_in->atom.type;
 
   LV2_ATOM_SEQUENCE_FOREACH(self->ports.midi_in, /*LV2_Atom_Event*/ ev) {
-    lv2_atom_sequence_append_event(self->ports.midi_out, out_capacity, ev);
+    if (ev->body.type == self->uris.midi_Event) {
+      const MIDIMsgEv *const in_msg = (MIDIMsgEv *)ev;
+      MIDIMsgEv out_msg;
+      for (int i = 0; i < REL_SIZE; i++) {
+        if (MIDI_match(&in_msg->msg, &self->rel.pairs[i].in)) {
+          out_msg.msg.statchan = self->rel.pairs[i].out.statchan;
+          out_msg.msg.data[0] = self->rel.pairs[i].out.data;
+          out_msg.msg.data[1] = in_msg->msg.data[1];
+          out_msg.ev.time = ev->time;
+          out_msg.ev.body.size = sizeof(MIDIMsg);
+          out_msg.ev.body.type = ev->body.type;
+          lv2_atom_sequence_append_event(self->ports.midi_out, out_capacity,
+                                         &out_msg.ev);
+        }
+        lv2_atom_sequence_append_event(self->ports.midi_out, out_capacity, ev);
+      }
+    }
   }
 }
 
---
+++ src/msgrel.h
@@ -0,0 +1,40 @@
+#ifndef MSGREL_H
+#define MSGREL_H
+
+#include <lv2/midi/midi.h>
+#include <stdint.h>
+
+#define REL_SIZE 2
+
+typedef struct {
+  uint8_t statchan;
+  uint8_t data[2];
+} MIDIMsg;
+
+typedef struct {
+  LV2_Atom_Event ev;
+  MIDIMsg msg;
+} MIDIMsgEv;
+
+// Specifies a single message
+typedef struct {
+  uint8_t statchan; // aaaabbbb where a is status (opcode) and b is channel
+  uint8_t data;     // ignores vel byte on note messages
+} VMSpec;           // Voice Message Specifier
+
+bool MIDI_match(const MIDIMsg *msg_real, const VMSpec *msg_spec) {
+  return msg_real->statchan == msg_spec->statchan &&
+         msg_real->data[0] == msg_spec->data;
+}
+
+typedef struct {
+  bool active;
+  VMSpec in;
+  VMSpec out;
+} VMPair; // Voice Message Pair
+
+typedef struct {
+  VMPair pairs[REL_SIZE];
+} VMBinRel;
+
+#endif // MSGREL_H
